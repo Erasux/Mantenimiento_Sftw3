52d18f0fedced571b7962f89de656af955824c71
Juan Andrés <juan.andresgiraldo@hotmail.com>
2023-12-10 15:12:13 -0500

    comentarios del componente chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import '../styles/chatbot.css'; 
 
+// Definición del componente llamado Chatbot
 function Chatbot() {
+    // Retorna un fragmento JSX que representa el componente de chatbot
     return (
-        <df-messenger
-            intent="WELCOME"
-            chat-title="MediBot"
-            agent-id="8a607da7-fffe-4202-95c2-b4ee10f31dcf"
-            language-code="es"
-     
-        ></df-messenger>
+      <df-messenger
+        // Intento inicial del chatbot 
+        intent="WELCOME"
+        
+        // Título que se mostrará en la interfaz del chatbot
+        chat-title="MediBot"
+        
+        // ID del agente asociado al chatbot 
+        agent-id="8a607da7-fffe-4202-95c2-b4ee10f31dcf"
+        
+        // Código de idioma para el chatbot (español en este caso)
+        language-code="es"
+      ></df-messenger>
     );
-}
-export default Chatbot;
+  }
+  
+  // Exporta el componente Chatbot para su uso en otras partes de la aplicación
+  export default Chatbot;
+  
